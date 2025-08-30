cace544ff44c5a2a979defc079769dfa93b76021
novaposkz <tanib@novapos.kz>
2025-08-30 06:51:16 +0500

    Update main.tf
    Change Public IP SKU from Basic to Standard to avoid quota limits

--- task04/main.tf
+++ task04/main.tf
@@ -67,6 +67,7 @@ resource "azurerm_public_ip" "main" {
   resource_group_name = azurerm_resource_group.main.name
   allocation_method   = "Static"
   domain_name_label   = var.dns_label
+  sku                 = "Standard"
   tags                = var.tags
 }
 
