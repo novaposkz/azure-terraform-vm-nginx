5e15032694dc528820c5b07abd8bf3cc75c2fc31
novaposkz <tanib@novapos.kz>
2025-08-30 06:12:21 +0500

    Update outputs.tf

--- task04/outputs.tf
+++ task04/outputs.tf
@@ -7,8 +7,3 @@ output "vm_fqdn" {
   description = "Fully Qualified Domain Name of the virtual machine"
   value       = azurerm_public_ip.main.fqdn
 }
-
-output "nginx_url" {
-  description = "URL to access Nginx default page"
-  value       = "http://${azurerm_public_ip.main.fqdn}"
-}
